4edb022f6fd929966ca0f5b9ebd58530cccf39c5
JorgeAcostaFreire <jorge.acosta.freire@gmail.com>
2022-07-30 18:12:47 +0200

    Initial Commit

---
+++ GifImage/GifImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  GifImageApp.swift
+//  GifImage
+//
+//  Created by Jorge Acosta Freire on 30/7/22.
+//
+
+import SwiftUI
+
+@main
+struct GifImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
